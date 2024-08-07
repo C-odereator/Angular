1a8c6fea8be738ce6b5f13493627dd77d3f09208
Priyanshu <priyanshuvsk09@gmail.com>
2024-08-07 21:00:41 +0530

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,MainComponent],
+  imports: [RouterOutlet,NavbarComponent,MainComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
