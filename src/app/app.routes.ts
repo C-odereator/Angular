d8a3d98b0df434b71d4037a87095af9d90f35beb
Priyanshu <priyanshuvsk09@gmail.com>
2024-08-10 20:17:59 +0530

    First

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component:MainComponent
+    },
+    {
+        path:'About', component:AboutComponent
+    },
+    {
+        path:'Admission', component:AdmissionComponent
+    }
+];
+
+// About","Admission","Learning","News & Events","Parents"
